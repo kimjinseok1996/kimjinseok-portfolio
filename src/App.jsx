40395a68142c745f76fe4cd2f3f53b072df5d4b5
unknown <kjs392@naver.com>
2025-07-05 22:16:17 +0900

    [TEST] vercel deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import ajax from '../src/assets/';
+import ajax from '../src/assets/ajax.webp';
 
 function App() {
   const [count, setCount] = useState(0)
